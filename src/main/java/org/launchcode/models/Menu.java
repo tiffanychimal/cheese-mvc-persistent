90aaa0a16895717f9a505f0c73eb1678b588ed4a
Tiffany Chimal <tiffanyc@makersvalley.net>
2019-03-02 12:35:17 +0100

    Revert "Initial commit"
    This reverts commit 67bd5af6f9c820749f606f188462ece07aaf2942.
    
    # Conflicts:
    #	src/main/java/org/launchcode/controllers/CheeseController.java
    #	src/main/resources/templates/fragments.html

--- src/main/java/org/launchcode/models/Menu.java
+++ src/main/java/org/launchcode/models/Menu.java
@@ -1,13 +1,15 @@
 package org.launchcode.models;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Category {
+public class Menu {
 
     @Id
     @GeneratedValue
@@ -17,17 +19,14 @@ public class Category {
     @Size(min=3, max=15)
     private String name;
 
-    @OneToMany
-    @JoinColumn(name = "category_id")
-    private List<Cheese> cheeses = new ArrayList<>();
+    @ManyToMany
+    private List<Cheese> cheeses;
 
-    public Category() { }
+    public Menu() {}
 
-    public Category(String name) { this.name = name; }
+    public void addItem(Cheese item) { cheeses.add(item); }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
 
     public String getName() { return name; }
 
